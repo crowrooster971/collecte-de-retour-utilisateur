af353508952af359f0bc022632d63b52757ddf0c
crowrooster971 <wanlishajna@hotmail.com>
2025-09-06 21:52:22 +0300

    Updated console message for server startup.

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use('/api/retours', retoursRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
