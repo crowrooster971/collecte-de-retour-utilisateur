ea405adda7cb9a8ba5a04f0217d31663bc239cc8
crowrooster971 <wanlishajna@hotmail.com>
2025-09-06 21:54:26 +0300

    Fix typo in welcome message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Bienvenue à l\'API de Collecte de Retours Utilisateur!');
+    res.send('Bienvenue à l\'API de Collecte de Retours Utilisateur !');
 });
 
 app.listen(PORT, () => {
